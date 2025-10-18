d6c1ad4fe0c33b1a2c1ca052283770baac7b44ea
John Okely <jlokely@gmail.com>
2025-10-18 14:58:19 +0800

    Coding style: wrap long class implements line to satisfy phpcs line-length

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -39,7 +39,9 @@ use core_privacy\local\request\writer;
  * @copyright  2018 Stephen Bourget
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\request\core_userlist_provider, \core_privacy\local\metadata\provider, \core_privacy\local\request\plugin\provider {
+class provider implements \core_privacy\local\request\core_userlist_provider,
+    \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\plugin\provider {
     /**
      * Return the fields which contain personal data.
      *
